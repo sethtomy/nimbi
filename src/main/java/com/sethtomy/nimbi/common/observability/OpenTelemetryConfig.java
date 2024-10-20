f440df40fface758c8fd30411a0f84b07541da46
Seth Tomy <sethtomy@gmail.com>
2024-10-20 18:03:11 -0400

    otel config

--- src/main/java/com/sethtomy/nimbi/common/observability/OpenTelemetryConfig.java
+++ src/main/java/com/sethtomy/nimbi/common/observability/OpenTelemetryConfig.java
@@ -1,13 +1,19 @@
 package com.sethtomy.nimbi.common.observability;
 
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @Configuration
 public class OpenTelemetryConfig {
+    @Value("${otel.host}")
+    private String host;
+
     @Bean
     public OtlpGrpcSpanExporter otlpGrpcSpanExporter() {
-        return OtlpGrpcSpanExporter.builder().setEndpoint("http://localhost:4317").build();
+        return OtlpGrpcSpanExporter.builder().setEndpoint(host).build();
     }
 }
